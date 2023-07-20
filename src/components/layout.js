6605708cd7cc8b8ef33e7939282eb0fedf709ae5
Skyz03 <aakibdiscoo@gmail.com>
2023-07-20 16:58:24 +0545

    part 2 completed with basic about css modules & components part

---
+++ src/components/layout.js
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import {
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText
+} from './layout.module.css'
+
+const Layout = ({ pageTitle, children }) => {
+  return (
+    <div className={container}>
+      <nav>
+        <ul className={navLinks}>
+          <li className={navLinkItem}><Link className={navLinkText} to="/">Home</Link></li>
+          <li className={navLinkItem}><Link className={navLinkText} to="/about">About</Link></li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default Layout
