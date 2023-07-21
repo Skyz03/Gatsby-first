224ef57b1f288f5c53e0eecf901474b7af2c9777
Skyz03 <aakibdiscoo@gmail.com>
2023-07-21 20:44:57 +0545

    update in part 6

---
+++ src/pages/blog/index.js
@@ -0,0 +1,41 @@
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
+
+const BlogPage = ({ data }) => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      <ul>
+        {
+          data.allMdx.nodes.map((node) => (
+            <article key={node.id}>
+              <h2>{node.frontmatter.title}</h2>
+              <p>Posted: {node.frontmatter.date}</p>
+              <p>{node.excerpt}</p>
+            </article>
+          ))
+        }
+      </ul>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+query {
+  allMdx(sort: {frontmatter: {date: DESC}}) {
+    nodes {
+      frontmatter {
+        date(formatString: "")
+        title
+      }
+      id
+      excerpt
+    }
+  }
+}
+`
+
+export const Head = () => <Seo title="My Blog Posts" />
+
+export default BlogPage
